04bd26a29630f1c03c0c8adbfd58e1c1ae4c6816
keighty <k80.leonard@gmail.com>
2025-10-16 15:57:58 -0700

    fix: broken links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Download
+            to="/docs/getting-started/getting-started">
+            Get Started
           </Link>
         </div>
       </div>
